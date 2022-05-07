ba5cd68ebd8eaf538690013a0867809192631d87
phonezgames <87576110+phonezgames@users.noreply.github.com>
2022-05-08 01:43:05 +0300

    Add files via upload

---
+++ ru/script.js
@@ -0,0 +1,157 @@
+$(function(){
+    var app = {
+        1 : {
+            name : "Unreal Escaping",
+            type : "Поймай хуйло",
+            art : "https://phonezgames.xyz/ru/icons/unrealescaping_event.png",
+            url : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/unrealescaping_event/",
+            update : {
+                version : "1.0 E",
+                date : "7 марта, 2022г.",
+                versionTxt : "&bull; Специальное событие, в поддержку Украины. Суть очень проста, вместо заключенного вы должны поймать сами понимаете кого))",
+                img : "https://phonezgames.xyz/ru/img/unrealescaping_event.png"
+},
+            details : {
+                seller : "PhoneZ Games",
+                size : "2,38 MB",
+                language : "Русский",
+                sort : "6+",
+                inapp : "Отсутствуют",
+                copy : "PhoneZ Games; 2022",
+                site : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/unrealescaping_event/"
+            }
+        },
+        2 : {
+            name : "Clash of Tanks",
+            type : "Уничтожь хуйло на танке",
+            art : "https://phonezgames.xyz/ru/icons/clashoftanks_event.png",
+            url : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/clashoftanks_event/",
+            update : {
+                version : "1.0 E",
+                date : "7 марта, 2022г.",
+                versionTxt : "Русский военный корабль - иди на#@й! Скорее всего, многие уже услышали легендарную фразу, так вот, давайте все вместе поддержим Украину, сыграв в специальное событие Clash of Tanks, где вы украинские танки, а победить вы должны российский!",
+                img : "https://phonezgames.xyz/ru/img/clashoftanks_event.png"
+},
+            details : {
+                seller : "PhoneZ Games",
+                size : "572 KB",
+                language : "Русский",
+                sort : "6+",
+                inapp : "Отсутствуют",
+                copy : "PhoneZ Games; 2022",
+                site : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/clashoftanks_event/"
+            }
+        },
+        3 : {
+            name : "Kill putins",
+            type : "Уничтожь всех 'путинов'",
+            art : "https://phonezgames.xyz/ru/icons/killputins.png",
+            url : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/killputins/",
+            update : {
+                version : "1.0",
+                date : "15 апреля, 2022г.",
+                versionTxt : "Просто нужно убить всех путинов в игре, тыкай играть и сам/а поймёшь))",
+                img : "https://phonezgames.xyz/ru/img/killputins.png"
+},
+            details : {
+                seller : "PhoneZ Games",
+                size : "84 KB",
+                language : "Русский",
+                sort : "6+",
+                inapp : "Отсутствуют",
+                copy : "PhoneZ Games; 2022",
+                site : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/killputins/"
+            }
+        },
+        4 : {
+            name : "Sniper 2D",
+            type : "Попади в цель",
+            art : "https://phonezgames.xyz/ru/icons/sniper2d.png",
+            url : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/sniper2d/",
+            update : {
+                version : "1.0",
+                date : "19 августа, 2022г.",
+                versionTxt : "Испытай насколько точно ты можешь попадать в цель онлайн!",
+                img : "https://phonezgames.xyz/ru/img/sniper2d.png"
+},
+            details : {
+                seller : "Github, PhoneZ Games reconstruction",
+                size : "24 KB",
+                language : "Русский",
+                sort : "6+",
+                inapp : "Отсутствуют",
+                copy : "PhoneZ Games; 2022",
+                site : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/sniper2d/"
+            }
+        },
+        5 : {
+            name : "Math Reaction",
+            type : "Развивает ментальную арифметику",
+            art : "https://phonezgames.xyz/ru/icons/mathreaction.png",
+            url : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/mathreaction/",
+            update : {
+                version : "1.0",
+                date : "9 августа, 2022г.",
+                versionTxt : "Игра про математические уравнения. Развивают логику и математическое мышление. Есть 3 типа игра: 'Обычный' - где у тебя 2 минуты на решения как можно больше примеров и в конце тебе высвечивается оценка по 5-ти бальной системе, 'Время' - где ты должен ответить на как можно больше примеров, если отвечаешь правильно - время прибавляется, неправильно - отнимается. Также есть самый сложный режим - 'Хардкор - где у тебя есть только 3 секунды дать ответ на лёгкий, а возможно и сложный пример. Ну же, испытай себя! ",
+                img : "https://phonezgames.xyz/ru/img/mathreaction.png"
+},
+            details : {
+                seller : "Github, PhoneZ Games Translations",
+                size : "52 KB",
+                language : "Русский",
+                sort : "4+",
+                inapp : "Отсутствуют",
+                copy : "PhoneZ Games; 2022",
+                site : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/mathreaction/"
+            }
+        },
+        6 : {
+            name : "Space Run",
+            type : "Управляй космической ракетой",
+            art : "https://phonezgames.xyz/ru/icons/spacerun.png",
+            url : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/spacerun/",
+            update : {
+                version : "1.0",
+                date : "24 марта, 2020г.",
+                versionTxt : "Ты - управляющий ракетой и попал под метеоритный дождь. Как долго он будет длится - неизвестно, как долго ты сможешь выжить в этой игре? Сыграй и узнай, вдруг ты прирождённый водитель ракетного судна?",
+                img : "https://phonezgames.xyz/ru/img/spacerun.png"
+},
+            details : {
+                seller : "Github, PhoneZ Games Translations",
+                size : "12 KB",
+                language : "Русский",
+                sort : "4+",
+                inapp : "Отсутствуют",
+                copy : "PhoneZ Games; 2022",
+                site : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/spacerun/"
+            }
+        },
+        7 : {
+            name : "SpeedRider",
+            type : "Бесконечная гоночная игра",
+            art : "https://phonezgames.xyz/ru/icons/speedrider.png",
+            url : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/speedrider/",
+            update : {
+                version : "1.1",
+                date : "7 Января, 2018г.",
+                versionTxt : "Это бесконечная гоночная компьютерная игра. Простое управление и смысл игры, 'залипалка' ещё та :)",
+                img : "https://phonezgames.xyz/ru/img/speedrider.png"
+},
+            details : {
+                seller : "Github, PhoneZ Games reconstruction",
+                size : "120 KB",
+                language : "Русский",
+                sort : "4+",
+                inapp : "Отсутствуют",
+                copy : "PhoneZ Games; 2022",
+                site : "http://adf.ly/20106767/https://phonezgames.xyz/ru/games/speedrider/"
+            }
+        }
+    }
+    var cout = '';
+    for (i in app) {
+    cout='<div id="'+app[i].name.toLowerCase().split(" ").join("").replace(/[^A-Za-z0-9 ]/g,"")+'" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><div class="modal-title"><div style="padding:15px;" align="left"><table class="app"><tr><td style="width:90px;"><img src="'+app[i].art+'" alt="'+app[i].name+'" draggable="false"></td><td style="padding-left:5px;"><h4><b>'+app[i].name+'</b></h4><h5 style="color:#7B7A7F;">'+app[i].type+'</h5></td><td style="width:60px;"><div id="#modalDismiss1" class="close" data-dismiss="modal" aria-label="Close"><svg width="25" height="25"><circle cx="12.5" cy="12.5" r="12.5" fill="#010101"></circle><path d="M 8.04 8.04 L16.07 16.07 M 8.04 16.07 L16.07 8.04 Z" style="stroke:#FFFFFF;  fill:none; stroke-width:3;" /></svg></div><div class="install"><a class="code" href="'+app[i].url+'"target="_blank">Играть</a></div></td></tr></table></div></div></div><div class="modal-body"><img src="'+app[i].update.img+'" class="inner"><hr><h5><span class="mod-first-letter"><h3><b>О игре</b></h3></span><h5>Версия '+app[i].update.version+'<br/>'+app[i].update.date+'</h5><h5 class="mod-first-letter">'+app[i].update.versionTxt+'</h5><hr><h5><span class="mod-first-letter"><h3><b>Подробнее</b></h3></span><div style="width:100%"><div class="mod-first-letter" align="left" style="padding:5px; position:absolute; left:10px;">Создатель</div><div align="right" style="padding:5px; position:absolute; right:10px;">'+app[i].details.seller+'</div><hr><div class="mod-first-letter" align="left" style="padding:5px; position:absolute; left:10px;">Размер</div><div align="right" style="padding:5px; position:absolute; right:10px;">'+app[i].details.size+'</div><hr><div class="mod-first-letter" align="left" style="padding:5px; position:absolute; left:10px;">Краткое описание</div><div align="right" style="padding:5px; position:absolute; right:10px;">'+app[i].type+'</div><hr><div class="mod-first-letter" align="left" style="padding:5px; position:absolute; left:10px;">Язык</div><div align="right" style="padding:5px; position:absolute; right:10px;">'+app[i].details.language+'</div><hr><div class="mod-first-letter" align="left" style="padding:5px; position:absolute; left:10px;">Возрастное ограничение</div><div align="right" style="padding:5px; position:absolute; right:10px;">'+app[i].details.sort+'</div><hr><div class="mod-first-letter" align="left" style="padding:5px; position:absolute; left:10px;">Покупки в игре</div><div align="right" style="padding:5px; position:absolute; right:10px;">'+app[i].details.inapp+'</div><hr><div class="mod-first-letter" align="left" style="padding:5px; position:absolute; left:10px;">Copyright</div><div align="right" style="padding:5px; position:absolute; right:10px;">'+app[i].details.copy+'</div><hr><div class="mod-first-letter" align="left" style="padding:5px; position:absolute; left:10px; color:rgb(0,122,252);"></div><hr></div></div></div></div></div>';
+        $(".store").append('<div class="col-lg-4 col-sm-6" align="center"><div class="thumbnail" data-toggle="modal" data-target="#'+app[i].name.toLowerCase().split(" ").join("").replace(/[^A-Za-z0-9 ]/g,"")+'" align="left"><table class="app"><tr><td style="width:90px;"><img src="'+app[i].art+'" alt="'+app[i].name+'" draggable="false"></td><td style="padding-left:5px;"><h4><b>'+app[i].name+'</b></h4><h5 style="color:#7B7A7F;">'+app[i].type+'</h5></td><td style="width:60px;"><div class="install"><a class="code" href="'+app[i].url+'" target="_blank">Играть</a></div></td></tr></table><hr id="hr"></div></div>'+cout);
+        cout='';
+    }
+});
