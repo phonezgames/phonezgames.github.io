54af2c8e95cf284b658b49cfa5bbbb63bdd10cba
phonezgames <87576110+phonezgames@users.noreply.github.com>
2022-05-08 21:42:09 +0300

    Update script.js

--- en/script.js
+++ en/script.js
@@ -21,11 +21,11 @@ $(function(){
                 site : "http://adf.ly/20106767/https://phonezgames.xyz/en/games/unrealescaping_event/"
             }
         },
-2 : {
+0 : {
             name : "Unreal Escaping",
             type : "Catch Lukashenko",
             art : "https://phonezgames.xyz/en/icons/unrealescaping_event_l.png",
-            url : "http://adf.ly/20106767/https://phonezgames.xyz/en/games/unrealescaping_event_l/",
+            url : "http://adf.ly/20106767/https://phonezgames.xyz/en/games/unrealescaping_l_event/",
             update : {
                 version : "1.0 E",
                 date : "May 8, 2022",
